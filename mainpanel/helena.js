b89172670a6417d0cbcab6b484ee1489457c02d5
schasins <schasins@gmail.com>
2018-02-04 21:44:52 -0800

    Adding bin op for reasoning about strings and string literals.

--- mainpanel/helena.js
+++ mainpanel/helena.js
@@ -1711,6 +1711,87 @@ var WebAutomationLanguage = (function _WebAutomationLanguage() {
     };
   };
 
+  pub.String = function _String(){
+    Revival.addRevivalLabel(this);
+    setBlocklyLabel(this, "string");
+    var stringFieldName = 'stringFieldName';
+    this.currentValue = null;
+
+    this.remove = function _remove(){
+      this.parent.removeChild(this);
+    }
+
+    this.prepareToRun = function _prepareToRun(){
+      return;
+    };
+    this.clearRunningState = function _clearRunningState(){
+      return;
+    }
+
+    this.toStringLines = function _toStringLines(){
+      if (this.nodeVar){
+        return [this.currentVal];
+      }
+      else{
+        return [""];
+      }
+    };
+
+    this.updateBlocklyBlock = function _updateBlocklyBlock(program, pageVars, relations){
+      addToolboxLabel(this.blocklyLabel, "text");
+      var defaultString = "your text here";
+      Blockly.Blocks[this.blocklyLabel] = {
+        init: function() {
+          var wal = getWAL(this);
+          if (!wal){
+            setWAL(this, new pub.String());
+          }
+          var statement = getWAL(this);
+
+          this.appendDummyInput()
+              .appendField(new Blockly.FieldTextInput(defaultString, function(newStr){statement.currentValue = newStr;}), stringFieldName);
+
+          this.setOutput(true, 'string');
+          this.setColour(25);
+          getWAL(this).currentValue = defaultString;
+        }
+      };
+    };
+
+    this.genBlocklyNode = function _genBlocklyNode(prevBlock, workspace){
+      this.block = workspace.newBlock(this.blocklyLabel);
+      setWAL(this.block, this);
+      if (this.currentValue){
+        this.block.setFieldValue(this.currentValue, stringFieldName);
+      }
+      return this.block;
+    };
+
+    this.getHelena = function _getHelena(){
+      return this;
+    };
+
+    this.traverse = function _traverse(fn, fn2){
+      fn(this);
+      fn2(this);
+    };
+
+    this.run = function _run(runObject, rbbcontinuation, rbboptions){
+      // it's just a constant.  no need to do anything
+    };
+
+    this.getCurrentVal = function _getCurrentVal(){
+      return this.currentValue;
+    };
+
+    this.parameterizeForRelation = function _parameterizeForRelation(relation){
+      return [];
+    };
+    this.unParameterizeForRelation = function _unParameterizeForRelation(relation){
+      return;
+    };
+  };
+
   pub.OutputRowStatement = function _OutputRowStatement(scrapeStatements){
     Revival.addRevivalLabel(this);
     setBlocklyLabel(this, "output");
@@ -2607,7 +2688,7 @@ var WebAutomationLanguage = (function _WebAutomationLanguage() {
        '==': function(a, b){ return a===b},
        '<': function(a, b){ return a<b},
        '<=': function(a, b){ return a<=b}
-    }
+    };
     var handleOpChange = function(newOp){
         if (this.sourceBlock_ && getWAL(this.sourceBlock_)){
           getWAL(this.sourceBlock_).operator = newOp;
@@ -2682,6 +2763,120 @@ var WebAutomationLanguage = (function _WebAutomationLanguage() {
 
       var leftVal = parseInt(this.left.getCurrentVal()); // todo: make this float not int
       var rightVal = parseInt(this.right.getCurrentVal());
+      this.currentVal = operators[this.operator](leftVal, rightVal);
+    };
+    this.getCurrentVal = function _getCurrentVal(){
+      return this.currentVal;
+    };
+    this.parameterizeForRelation = function _parameterizeForRelation(relation){
+      return [];
+    };
+    this.unParameterizeForRelation = function _unParameterizeForRelation(relation){
+      return;
+    };
+  };
+
+  pub.BinOpString = function _BinOpString(){
+    Revival.addRevivalLabel(this);
+    setBlocklyLabel(this, "binopstring");
+    this.left = null;
+    this.right = null;
+    this.operator = null;
+
+    this.remove = function _remove(){
+      this.parent.removeChild(this);
+    }
+
+    this.prepareToRun = function _prepareToRun(){
+      return;
+    };
+    this.clearRunningState = function _clearRunningState(){
+      return;
+    }
+
+    this.toStringLines = function _toStringLines(){
+      return ["binopstring"];
+    };
+
+    var operators = {
+       'contains': function(a, b){ return a.indexOf(b) > -1; },
+       'is in': function(a, b){ return b.indexOf(a) > -1; },
+       'is': function(a, b){ return a === b; }
+    };
+    var handleOpChange = function(newOp){
+        if (this.sourceBlock_ && getWAL(this.sourceBlock_)){
+          getWAL(this.sourceBlock_).operator = newOp;
+        }
+    };
+
+    this.updateBlocklyBlock = function _updateBlocklyBlock(program, pageVars, relations){
+      var dropdown = makeOpsDropdown(operators);
+      addToolboxLabel(this.blocklyLabel, "text");
+      Blockly.Blocks[this.blocklyLabel] = {
+        init: function() {
+          this.appendValueInput("left");
+          this.appendDummyInput().appendField(new Blockly.FieldDropdown(dropdown, handleOpChange), "op");
+          this.appendValueInput("right");
+          this.setInputsInline(true);
+          this.setOutput(true, 'Bool');
+          this.setColour(25);
+
+          var wal = getWAL(this);
+          if (!wal){
+            setWAL(this, new pub.BinOpString());
+            var op = dropdown[0][0];
+            getWAL(this).operator = op; // since this is what it'll show by default, better act as though that's true
+          }
+        }
+      };
+    };
+
+    this.genBlocklyNode = function _genBlocklyNode(prevBlock, workspace){
+      this.block = workspace.newBlock(this.blocklyLabel);
+      setWAL(this.block, this);
+      this.block.setFieldValue(this.operator, "op");
+      if (this.left){
+        attachToInput(this.block, this.left.genBlocklyNode(this.block, workspace), "left");
+      }
+      if (this.right){
+        attachToInput(this.block, this.right.genBlocklyNode(this.block, workspace), "right");
+      }
+      return this.block;
+    };
+
+    this.getHelena = function _getHelena(){
+      // ok, but we also want to update our own condition object
+      var leftBlock = this.block.getInput('left').connection.targetBlock();
+      var rightBlock = this.block.getInput('right').connection.targetBlock();
+      if (leftBlock){
+        this.left = getWAL(leftBlock).getHelena();
+      }
+      else{
+        this.left = null;
+      }
+      if (rightBlock){
+        this.right = getWAL(rightBlock).getHelena();
+      }
+      else{
+        this.right = null;
+      }
+      return this;
+    };
+
+    this.traverse = function _traverse(fn, fn2){
+      fn(this);
+      if (this.left){this.left.traverse(fn, fn2);}
+      if (this.right){ this.right.traverse(fn, fn2);}
+      fn2(this);
+    };
+
+    this.run = function _run(runObject, rbbcontinuation, rbboptions){
+      // now run the things on which we depend
+      this.left.run(runObject, rbbcontinuation, rbboptions);
+      this.right.run(runObject, rbbcontinuation, rbboptions);
+
+      var leftVal = this.left.getCurrentVal(); // todo: make this float not int
+      var rightVal = this.right.getCurrentVal();
       this.currentVal = operators[this.operator](leftVal, rightVal);
     };
     this.getCurrentVal = function _getCurrentVal(){
