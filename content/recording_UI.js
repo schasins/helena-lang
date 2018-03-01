daa4ab174d31f65a6494f755c0406a69b33f9ee2
schasins <schasins@gmail.com>
2018-03-01 15:57:50 -0800

    Update recording_UI.js
    Just expanding the comment a little -- but loving the thorough commenting!  Keep it up.  :)

--- content/recording_UI.js
+++ content/recording_UI.js
@@ -263,13 +263,17 @@ var Scraping = (function _Scraping() { var pub = {};
 
   let currKeyState = {}; // keeps track of the keys that are currently being pressed down
 
-  // Because Windows has this habit of recording multiple events for a continued 
-  // key press or key down, we want to throw out events that are repeats of events
+  // Because Windows has this habit of producing multiple keypress, keydown events 
+  // for a continued key press, we want to throw out events that are repeats of events
   // we've already seen. This change fixes a major issue with running Helena
-  // on Windows, in that scraping gets hung up on all the key presses instead
-  // of gathering data really fast. 
+  // on Windows, in that the top-level tool chooses to ignore events that can be
+  // accomplished without running Ringer (e.g. scraping relation items), but keydown
+  // events can only be accomplished by Ringer.  So replay gets slow because of having to
+  // replay all the Ringer events for each row of the relation.
+  // note: if we run into issues where holding a key down in a recording produces a bad replay,
+  // look here first
   additional_recording_filters.ignoreExtraKeydowns = function(eventData){
-    if (eventData.type === "keypress" || eventData.type === "keydown") { // for now, we'll ignore multiple key presses for all keys (not just ctrl and alt)
+    if (eventData.type === "keypress" || eventData.type === "keydown") { // for now, we'll ignore multiple keypresses for all keys (not just ctrl and alt)
       if (!currKeyState[eventData.keyCode]) { // first seen, record that it's being pressed down
         currKeyState[eventData.keyCode] = true;
         return false;
