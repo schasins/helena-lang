e1868cd0abef6f76a5e86010218747ba803be2cb
schasins <schasins@gmail.com>
2018-09-15 20:20:38 -0700

    Can now check length of strings.

--- mainpanel/helena.js
+++ mainpanel/helena.js
@@ -2981,6 +2981,89 @@ var WebAutomationLanguage = (function _WebAutomationLanguage() {
     };
   };
 
+  pub.LengthString = function _LengthString(){
+    Revival.addRevivalLabel(this);
+    setBlocklyLabel(this, "lengthstring");
+    this.input = null;
+
+    this.remove = function _remove(){
+      this.parent.removeChild(this);
+    }
+
+    this.prepareToRun = function _prepareToRun(){
+      return;
+    };
+    this.clearRunningState = function _clearRunningState(){
+      return;
+    }
+
+    this.toStringLines = function _toStringLines(){
+      return ["lengthstring"];
+    };
+
+    this.updateBlocklyBlock = function _updateBlocklyBlock(program, pageVars, relations){
+      addToolboxLabel(this.blocklyLabel, "text");
+      Blockly.Blocks[this.blocklyLabel] = {
+        init: function() {
+          this.appendDummyInput()
+              .appendField("length of");
+          this.appendValueInput("input");
+          this.setInputsInline(true);
+          this.setOutput(true, 'Bool');
+          this.setColour(25);
+
+          var wal = getWAL(this);
+          if (!wal){
+            setWAL(this, new pub.LengthString());
+          }
+        }
+      };
+    };
+
+    this.genBlocklyNode = function _genBlocklyNode(prevBlock, workspace){
+      this.block = workspace.newBlock(this.blocklyLabel);
+      setWAL(this.block, this);
+      if (this.input){
+        attachToInput(this.block, this.input.genBlocklyNode(this.block, workspace), "input");
+      }
+      return this.block;
+    };
+
+    this.getHelena = function _getHelena(){
+      // ok, but we also want to update our own condition object
+      var inputBlock = this.block.getInput('input').connection.targetBlock();
+      if (inputBlock){
+        this.input = getWAL(inputBlock).getHelena();
+      }
+      else{
+        this.input = null;
+      }
+      return this;
+    };
+
+    this.traverse = function _traverse(fn, fn2){
+      fn(this);
+      if (this.input){this.input.traverse(fn, fn2);}
+      fn2(this);
+    };
+
+    this.run = function _run(runObject, rbbcontinuation, rbboptions){
+      // now run the things on which we depend
+      this.input.run(runObject, rbbcontinuation, rbboptions);
+      var inputVal = this.input.getCurrentVal();
+      this.currentVal = inputVal.length;
+    };
+    this.getCurrentVal = function _getCurrentVal(){
+      return this.currentVal;
+    };
+    this.parameterizeForRelation = function _parameterizeForRelation(relation){
+      return [];
+    };
+    this.unParameterizeForRelation = function _unParameterizeForRelation(relation){
+      return;
+    };
+  };
+
   var SkippingStrategies = {
     ALWAYS: "always",
     NEVER: "never",
@@ -6868,7 +6951,7 @@ var WebAutomationLanguage = (function _WebAutomationLanguage() {
 
     // this is silly, but just making a new object for each of our statements is an easy way to get access to
     // the updateBlocklyBlock function and still keep it an instance method/right next to the genBlockly function
-    var toolBoxBlocks = ["Number", "NodeVariableUse", "String", "Concatenate", "IfStatement", "ContinueStatement", "BinOpString", "BinOpNum",
+    var toolBoxBlocks = ["Number", "NodeVariableUse", "String", "Concatenate", "IfStatement", "ContinueStatement", "BinOpString", "BinOpNum", "LengthString",
       "BackStatement", "ClosePageStatement", "WaitStatement", "WaitUntilUserReadyStatement", "SayStatement"];
     // let's also add in other nodes which may not have been used in programs so far, but which we want to include in the toolbox no matter what
     var origBlocks = blocklyNames;
