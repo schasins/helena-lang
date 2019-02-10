ab86edff3a44c395de99bb2f8a3e1ffcc9ca3e35
schasins <schasins@gmail.com>
2019-02-10 11:55:20 -0800

    Handle the fact that some of the programs we might want to download are very big.

--- common/misc_utilities.js
+++ common/misc_utilities.js
@@ -929,7 +929,9 @@ var DownloadUtilities = (function _DownloadUtilities() { var pub = {};
 
   pub.download = function _download(filename, text) {
     var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    //element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('href', URL.createObjectURL(new Blob([text], {
+                  type: "application/octet-stream"})));
     element.setAttribute('download', filename);
 
     element.style.display = 'none';
